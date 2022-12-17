d0635d4b286e95151686eeefc084c0d52256fcba
lukaasz555 <lukasz.zegar555@gmail.com>
2022-12-17 14:16:29 +0100

    tidyup

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import configureStore from './store/configureStore';
-import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<Provider store={configureStore()}>
-			<Router>
-				<App />
-			</Router>
-		</Provider>
+		<Router>
+			<App />
+		</Router>
 	</React.StrictMode>
 );
