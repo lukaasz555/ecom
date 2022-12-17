d1d8e4c8f66141f4debafc67bd071958c3efae6d
lukaasz555 <lukasz.zegar555@gmail.com>
2022-12-17 14:04:42 +0100

    redux init

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<Router>
-			<App />
-		</Router>
+		<Provider store={configureStore()}>
+			<Router>
+				<App />
+			</Router>
+		</Provider>
 	</React.StrictMode>
 );
