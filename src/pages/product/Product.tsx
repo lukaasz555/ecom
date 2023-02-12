8a7d853fd94c5bd55cc212284c60e1d44eb341c7
lukaasz555 <lukasz.zegar555@gmail.com>
2023-02-12 11:03:27 +0100

    fetching product

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -1,14 +1,11 @@
 import { useEffect, useState, useRef } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import ProductLayout from '../../components/templates/ProductLayout/ProductLayout';
 import Layout from '../../components/templates/Layout/Layout';
 import { ProductModel } from '../../models/Product';
 import ProductHead from '../../components/organisms/ProductHead/ProductHead';
 import ProductDesc from '../../components/atoms/ProductDesc/ProductDesc';
 import ProductDetails from '../../components/atoms/ProductDetails/ProductDetails';
-//import { books } from '../../data/books';
-//import { albums } from '../../data/albums';
-//import { getCurrentProduct } from '../../helpers/getCurrentProduct';
 import axios from 'axios';
 import Return from '../../components/atoms/Return/Return';
 import ProductModal from '../../components/atoms/ProductModal/ProductModal';
@@ -33,7 +30,6 @@ const Product = () => {
 	const [product, setProduct] = useState<ProductModel>(initValue);
 	const myRef = useRef(null);
 	const location = useLocation();
-	const category = location.pathname.replace('/shop/product/', '');
 	const [showModal, setShowModal] = useState(false);
 	const [isLoading, setLoading] = useState(true);
 	const URL = process.env.REACT_APP_SERVER_URL;
@@ -43,26 +39,16 @@ const Product = () => {
 	const openModal = () => setShowModal(true);
 	const closeModal = () => setShowModal(false);
 
-	const getId = () => {
-		if (category.includes('books')) {
-			const id = category.replace('books/', '');
-			return id;
-		} else if (category.includes('albums')) {
-			const id = category.replace('albums/', '');
-			return id;
-		}
-	};
+	const { id } = useParams();
 
 	useEffect(() => {
-		const productId = getId();
 		axios
-			.get(`${URL}/products/${productId}`, {
+			.get(`${URL}/products/${id}`, {
 				params: {
-					id: productId,
+					id,
 				},
 			})
 			.then((res) => {
-				console.log(res);
 				setProduct(res.data);
 				setLoading(false);
 				setError(false);
@@ -72,18 +58,29 @@ const Product = () => {
 				setLoading(false);
 				setError(true);
 			});
-		console.log(product.description);
 	}, [location]);
 
 	return (
 		<Layout>
 			{isLoading ? (
-				<div className='min-h-[400px] flex justify-center items-center'>
+				<div className='min-h-[50vh] flex justify-center items-center'>
 					<Loader />
 				</div>
 			) : status === 204 ? (
-				<div className='min-h-[400px] flex justify-center items-center'>
-					<h1 className='text-center'>Strona nie istnieje</h1>
+				<div className='min-h-[50vh] flex flex-col justify-center items-center'>
+					<h1 className='text-center'>Brak produktu o podanym ID</h1>
+					<svg
+						width='22'
+						height='26'
+						viewBox='0 0 22 26'
+						fill='none'
+						xmlns='http://www.w3.org/2000/svg'>
+						<circle cx='11' cy='11' r='10.5' stroke='#140004' />
+						<path
+							d='M11.576 5.36364L11.5021 13.6648H10.5021L10.4283 5.36364H11.576ZM11.0021 17.0852C10.7786 17.0852 10.5855 17.0057 10.4226 16.8466C10.2635 16.6837 10.1839 16.4905 10.1839 16.267C10.1839 16.0398 10.2635 15.8466 10.4226 15.6875C10.5855 15.5284 10.7786 15.4489 11.0021 15.4489C11.2256 15.4489 11.4169 15.5284 11.576 15.6875C11.7389 15.8466 11.8203 16.0398 11.8203 16.267C11.8203 16.4148 11.7824 16.5511 11.7067 16.6761C11.6347 16.8011 11.5362 16.9015 11.4112 16.9773C11.29 17.0492 11.1536 17.0852 11.0021 17.0852Z'
+							fill='#140004'
+						/>
+					</svg>
 				</div>
 			) : status === 200 ? (
 				<>
@@ -116,11 +113,13 @@ const Product = () => {
 					/>
 				</>
 			) : (
-				<div className='min-h-[400px] flex justify-center items-center'>
-					<h1 className='text-center'>
-						Problem z pobraniem zawartości. <br /> Spróbuj ponownie
-					</h1>
-				</div>
+				error && (
+					<div className='min-h-[400px] flex justify-center items-center'>
+						<h1 className='text-center'>
+							Problem z pobraniem zawartości. <br /> Spróbuj ponownie
+						</h1>
+					</div>
+				)
 			)}
 		</Layout>
 	);
