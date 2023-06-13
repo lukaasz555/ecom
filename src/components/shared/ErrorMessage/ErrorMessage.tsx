8a8cfbb832726a8abee5649ae183963526fa86e2
lukaasz555 <lukasz.zegar555@gmail.com>
2023-06-13 07:17:59 +0200

    account/my-orders error handlin

--- src/components/shared/ErrorMessage/ErrorMessage.tsx
+++ src/components/shared/ErrorMessage/ErrorMessage.tsx
@@ -1,31 +1,60 @@
 type ErrorMessageProps = {
 	text1: string;
 	text2?: string;
+	align?: string;
 };
 
-const ErrorMessage = ({ text1, text2 }: ErrorMessageProps) => {
+const ErrorMessage = ({
+	text1,
+	text2,
+	align = 'center',
+}: ErrorMessageProps) => {
 	return (
-		<div className='w-[100%] min-h-[200px] flex justify-center items-center'>
-			<p className='text-brownSugar text-center'>
-				{text1}
-				<br />
-				{text2}
-				<div className='flex justify-center mt-0.5'>
-					<svg
-						width='22'
-						height='26'
-						viewBox='0 0 22 26'
-						fill='none'
-						xmlns='http://www.w3.org/2000/svg'>
-						<circle cx='11' cy='11' r='10.5' stroke='#B96D40' />
-						<path
-							d='M11.576 5.36364L11.5021 13.6648H10.5021L10.4283 5.36364H11.576ZM11.0021 17.0852C10.7786 17.0852 10.5855 17.0057 10.4226 16.8466C10.2635 16.6837 10.1839 16.4905 10.1839 16.267C10.1839 16.0398 10.2635 15.8466 10.4226 15.6875C10.5855 15.5284 10.7786 15.4489 11.0021 15.4489C11.2256 15.4489 11.4169 15.5284 11.576 15.6875C11.7389 15.8466 11.8203 16.0398 11.8203 16.267C11.8203 16.4148 11.7824 16.5511 11.7067 16.6761C11.6347 16.8011 11.5362 16.9015 11.4112 16.9773C11.29 17.0492 11.1536 17.0852 11.0021 17.0852Z'
-							fill='#B96D40'
-						/>
-					</svg>
+		<>
+			{align === 'center' ? (
+				<div className='w-[100%] min-h-[200px] flex justify-center items-center'>
+					<div className='text-brownSugar text-center'>
+						<p>{text1}</p>
+						<p>{text2}</p>
+						<div className='flex justify-center mt-0.5'>
+							<svg
+								width='22'
+								height='26'
+								viewBox='0 0 22 26'
+								fill='none'
+								xmlns='http://www.w3.org/2000/svg'>
+								<circle cx='11' cy='11' r='10.5' stroke='#B96D40' />
+								<path
+									d='M11.576 5.36364L11.5021 13.6648H10.5021L10.4283 5.36364H11.576ZM11.0021 17.0852C10.7786 17.0852 10.5855 17.0057 10.4226 16.8466C10.2635 16.6837 10.1839 16.4905 10.1839 16.267C10.1839 16.0398 10.2635 15.8466 10.4226 15.6875C10.5855 15.5284 10.7786 15.4489 11.0021 15.4489C11.2256 15.4489 11.4169 15.5284 11.576 15.6875C11.7389 15.8466 11.8203 16.0398 11.8203 16.267C11.8203 16.4148 11.7824 16.5511 11.7067 16.6761C11.6347 16.8011 11.5362 16.9015 11.4112 16.9773C11.29 17.0492 11.1536 17.0852 11.0021 17.0852Z'
+									fill='#B96D40'
+								/>
+							</svg>
+						</div>
+					</div>
 				</div>
-			</p>
-		</div>
+			) : (
+				<div className='w-[100%] flex justify-start gap-x-4 items-center'>
+					<div>
+						<svg
+							width='22'
+							height='26'
+							viewBox='0 0 22 26'
+							fill='none'
+							xmlns='http://www.w3.org/2000/svg'>
+							<circle cx='11' cy='11' r='10.5' stroke='#B96D40' />
+							<path
+								d='M11.576 5.36364L11.5021 13.6648H10.5021L10.4283 5.36364H11.576ZM11.0021 17.0852C10.7786 17.0852 10.5855 17.0057 10.4226 16.8466C10.2635 16.6837 10.1839 16.4905 10.1839 16.267C10.1839 16.0398 10.2635 15.8466 10.4226 15.6875C10.5855 15.5284 10.7786 15.4489 11.0021 15.4489C11.2256 15.4489 11.4169 15.5284 11.576 15.6875C11.7389 15.8466 11.8203 16.0398 11.8203 16.267C11.8203 16.4148 11.7824 16.5511 11.7067 16.6761C11.6347 16.8011 11.5362 16.9015 11.4112 16.9773C11.29 17.0492 11.1536 17.0852 11.0021 17.0852Z'
+								fill='#B96D40'
+							/>
+						</svg>
+					</div>
+					<div className='text-brownSugar'>
+						<div>{text1}</div>
+						<div>{text2 && text2}</div>
+					</div>
+				</div>
+			)}
+		</>
 	);
 };
 
--- src/modules/Shop/components/organisms/OrdersHistory/OrdersHistory.tsx
+++ src/modules/Shop/components/organisms/OrdersHistory/OrdersHistory.tsx
@@ -4,16 +4,26 @@ import { fetchUserOrders } from '../../../../../services/orders.service';
 import { OrderModel } from '../../../../../models/Order';
 import { handleNumbFormat } from '../../../../../helpers/handleNumbFormat';
 import moment from 'moment';
+import Loader from '../../../../../components/shared/Loader/Loader';
+import ErrorMessage from '../../../../../components/shared/ErrorMessage/ErrorMessage';
 
 const OrdersHistory = () => {
 	const user = useAppSelector((state) => state.userReducer.user);
+	const [isLoading, setLoading] = useState(false);
+	const [isError, setError] = useState(false);
 	const [usersOrder, setUsersOrder] = useState<OrderModel[]>([]);
 
 	useEffect(() => {
 		if (user) {
-			fetchUserOrders(user.id).then((res) => {
-				setUsersOrder(res);
-			});
+			setLoading(true);
+			fetchUserOrders(user.id)
+				.then((res) => {
+					if (Array.isArray(res)) {
+						setUsersOrder(res);
+						setError(false);
+					} else setError(true);
+				})
+				.finally(() => setLoading(false));
 		}
 	}, [user]);
 
@@ -21,17 +31,27 @@ const OrdersHistory = () => {
 		<>
 			<div className='flex flex-col items-start m-4 mt-0 ml-8'>
 				<h2 className='uppercase text-xl font-lato mb-6'>historia zakupów</h2>
-				{Array.isArray(usersOrder) && usersOrder.length > 0 ? (
-					usersOrder.map((x) => (
-						<div key={x._id} className='flex gap-x-4'>
-							<p>{moment(x.createdAt).format('DD-MM-YYYY')}</p>
-							<p className='font-medium'>Nr zam: {x._id}</p>
-							<p>kwota - {handleNumbFormat(x.order.value)} zł</p>
-						</div>
-					))
-				) : (
-					<p>Brak zamówień w historii</p>
-				)}
+				<div className=''>
+					{isLoading ? (
+						<Loader size='medium' />
+					) : isError ? (
+						<ErrorMessage
+							text1='Nie udało pobrać się zawartości'
+							text2='Spróbuj ponownie'
+							align='left'
+						/>
+					) : Array.isArray(usersOrder) && usersOrder.length > 0 ? (
+						usersOrder.map((x) => (
+							<div key={x._id} className='flex gap-x-4'>
+								<p>{moment(x.createdAt).format('DD-MM-YYYY')}</p>
+								<p className='font-medium'>Nr zam: {x._id}</p>
+								<p>kwota - {handleNumbFormat(x.order.value)} zł</p>
+							</div>
+						))
+					) : !Array.isArray(usersOrder) ? (
+						<p>Brak zamówień w historii</p>
+					) : null}
+				</div>
 			</div>
 		</>
 	);
