a147d70e27844de171adb770428fc7f6e0ee13c4
lukaasz555 <lukasz.zegar555@gmail.com>
2023-07-08 14:37:09 +0200

    password update - api

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,10 +2,32 @@ import { User } from '../models/User';
 import { ApiUserResponse } from '../models/api';
 import api from '../utils/api';
 
+interface ChangePassword {
+	email: string;
+	password: string;
+	newPassword: string;
+}
+
 export async function edit(user: User): Promise<ApiUserResponse> {
 	return await api
 		.put('user', {
 			user,
+		})
+		.then((res) => {
+			return {
+				status: res.status,
+				token: res.data,
+			};
+		})
+		.catch((e) => ({ status: e.response.status }));
+}
+
+export async function changePassword(
+	data: ChangePassword
+): Promise<ApiUserResponse> {
+	return await api
+		.put('user/password', {
+			...data,
 		})
 		.then((res) => {
 			return {
